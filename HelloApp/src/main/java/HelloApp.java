f9fbf5f5302add7a8e92c0287e6c5dde0e8b6449
mn9508-git <mn9508@srmist.edu.in>
2026-03-25 13:55:09 +0530

    UC4: Display Hello with multiple names

--- HelloApp/src/main/java/HelloApp.java
+++ HelloApp/src/main/java/HelloApp.java
@@ -1,15 +1,15 @@
 public class HelloApp {
     public static void main(String[] args) {
 
-        String name;
-
-        if (args.length > 0) {
-            name = args[0];
+        if (args.length == 0) {
+            System.out.println("Hello World");
         } else {
-            name = "World";
+            System.out.print("Hello ");
+            for (int i = 0; i < args.length; i++) {
+                System.out.print(args[i] + " ");
+            }
         }
 
-        System.out.println("Hello " + name);
     }
 }
 
