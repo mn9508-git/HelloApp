a0a728a9c7dbfbccf6ee2955db2e392edcb20760
mn9508-git <mn9508@srmist.edu.in>
2026-03-25 13:42:54 +0530

    UC2:Display Hello with command line argument

--- HelloApp/src/main/java/HelloApp.java
+++ HelloApp/src/main/java/HelloApp.java
@@ -1,9 +1,14 @@
 public class HelloApp {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+        if (args.length > 0) {
+            System.out.println("Hello " + args[0]);
+        } else {
+            System.out.println("Hello World");
+        }
+
     }
 }
-
 
 
 
